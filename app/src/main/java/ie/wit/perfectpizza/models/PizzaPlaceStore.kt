a23019f11cef2d813f7b10dc1e67962ef621e2d4
sharonpower <20084746@mail.wit.ie>
2022-03-23 17:05:07 +0000

    refactor part2

--- app/src/main/java/ie/wit/perfectpizza/models/PizzaPlaceStore.kt
+++
@@ -1,8 +0,0 @@
-package ie.wit.perfectpizza.models
-
-interface PizzaPlaceStore {
-    fun findAll(): List<PizzaPlaceModel>
-    fun create(pizzaplace: PizzaPlaceModel)
-    fun update(pizzaplace: PizzaPlaceModel)
-    fun delete(pizzaplace: PizzaPlaceModel)
-}
