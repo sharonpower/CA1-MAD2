ebe312a7bfacdbc4ed80aee3abcfa836c1f9b39e
sharonpower <20084746@mail.wit.ie>
2022-03-23 20:39:16 +0000

    colours adjusted, home page and security page

--- app/src/main/java/ie/wit/piggybank/ui/grouppiggybank/GroupPiggyBankFragment.kt
+++ app/src/main/java/ie/wit/piggybank/ui/grouppiggybank/GroupPiggyBankFragment.kt
@@ -13,8 +13,6 @@ class GroupPiggyBankFragment : Fragment() {
 
     private var _binding: FragmentGrouppiggybankBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
